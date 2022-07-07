012b99e6bfaadf6e4993b81b612d9f9097f85d0c
Akshay Kumar Kushwaha <76196722+Akshay01-M@users.noreply.github.com>
2022-07-07 15:25:58 +0530

    disconnect main page

--- App/hers/lib/main.dart
+++ App/hers/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hers/screens/firstScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const FirstScreen(),
     );
   }
 }
