4b9e79676cd2035d15bd46b2ee39f15bb430aea3
Akshay Kumar Kushwaha <76196722+Akshay01-M@users.noreply.github.com>
2022-07-08 00:28:58 +0530

    second screen complete

--- App/hers/lib/screens/secondScreen.dart
+++ App/hers/lib/screens/secondScreen.dart
@@ -1,30 +1,97 @@
+import 'package:camera/camera.dart';
+import 'package:tflite/tflite.dart';
 import 'package:flutter/material.dart';
+import 'package:hers/main.dart';
 import 'package:hers/screens/firstScreen.dart';
 
-class SecondScreen extends StatelessWidget {
-  const SecondScreen({Key? key, Text? title}) : super(key: key);
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text("Second Screen"),
-          centerTitle: true,
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<SecondScreen> {
+  CameraImage? cameraImage;
+  CameraController? cameraController;
+  String output = '';
+
+  @override
+  void initState() {
+    super.initState();
+    loadCamera();
+    loadModel();
+  }
+
+  loadCamera() {
+    cameraController = CameraController(cameras![0], ResolutionPreset.medium);
+    cameraController!.initialize().then((value) {
+      if (!mounted) {
+        return;
+      } else {
+        setState(() {
+          cameraController!.startImageStream((imageStream) {
+            cameraImage = imageStream;
+            runModel();
+          });
+        });
+      }
+    });
+  }
+
+  runModel() async {
+    if (cameraImage != null) {
+      var predictions = await Tflite.runModelOnFrame(
+          bytesList: cameraImage!.planes.map((plane) {
+            return plane.bytes;
+          }).toList(),
+          imageHeight: cameraImage!.height,
+          imageWidth: cameraImage!.width,
+          imageMean: 127.5,
+          imageStd: 127.5,
+          rotation: 90,
+          numResults: 2,
+          threshold: 0.1,
+          asynch: true);
+
+      predictions!.forEach((element) {
+        setState(() {
+          output = element['label'];
+        });
+      });
+    }
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: "assets/model/model.tflite", labels: "assets/model/labels.txt");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recognition using Camera'),
+      ),
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.7,
+            width: MediaQuery.of(context).size.width,
+            child: !cameraController!.value.isInitialized
+                ? Container()
+                : AspectRatio(
+                    aspectRatio: cameraController!.value.aspectRatio,
+                    child: CameraPreview(cameraController!),
+                  ),
+          ),
         ),
-        body: Center(
-            child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.all(20),
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text(
-            'Back Screen',
-            style: TextStyle(
-              fontSize: 10,
-            ),
-          ),
-        )),
-      );
+        Text(
+          output,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        )
+      ]),
+    );
+  }
 }
