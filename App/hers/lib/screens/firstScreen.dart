7d532841550fd8b86996c9c2fa5cf6695e108266
Akshay Kumar Kushwaha <76196722+Akshay01-M@users.noreply.github.com>
2022-07-07 15:38:37 +0530

    add third Screen

--- App/hers/lib/screens/firstScreen.dart
+++ App/hers/lib/screens/firstScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hers/screens/secondScreen.dart';
+import 'package:hers/screens/thirdScreen.dart';
 
 class FirstScreen extends StatelessWidget {
   const FirstScreen({Key? key, Text? title}) : super(key: key);
@@ -12,20 +13,48 @@ class FirstScreen extends StatelessWidget {
           centerTitle: true,
         ),
         body: Center(
-            child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.all(20),
+          child: Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(20),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SecondScreen()));
+                  },
+                  child: const Text(
+                    'Second Screen',
+                    style: TextStyle(
+                      fontSize: 10,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(20),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ThirdScreen()));
+                  },
+                  child: const Text(
+                    'Third Screen',
+                    style: TextStyle(
+                      fontSize: 10,
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const SecondScreen()));
-          },
-          child: const Text(
-            'Next Screen',
-            style: TextStyle(
-              fontSize: 10,
-            ),
-          ),
-        )),
+        ),
       );
 }
