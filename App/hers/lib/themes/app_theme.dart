9737ead5a95695eb6694260cefd17d396fac9c20
Akshay Kumar Kushwaha <76196722+Akshay01-M@users.noreply.github.com>
2022-07-21 15:58:17 +0530

    app theme

---
+++ App/hers/lib/themes/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:hers/themes/dark_theme.dart';
+import 'package:hers/themes/light_theme.dart';
+
+class AppTheme {
+  static ThemeData light = lightTheme;
+  static ThemeData dark = darkTheme;
+}
