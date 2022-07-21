a9c5850a60b31e5486bcbdfc25cb8e2648f7af26
Akshay Kumar Kushwaha <76196722+Akshay01-M@users.noreply.github.com>
2022-07-22 00:10:52 +0530

    Change in first page ui

--- App/hers/lib/screens/firstScreen.dart
+++ App/hers/lib/screens/firstScreen.dart
@@ -3,78 +3,102 @@ import 'package:hers/screens/handGesture.dart';
 import 'package:hers/screens/secondScreen.dart';
 import 'package:hers/screens/thirdScreen.dart';
 
-class FirstScreen extends StatelessWidget {
-  const FirstScreen({Key? key, Text? title}) : super(key: key);
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text("First Screen"),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Row(
+  _FirstScreen createState() => _FirstScreen();
+}
+
+class _FirstScreen extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          width: size.width,
+          height: size.height,
+          padding:
+              const EdgeInsets.only(left: 20, right: 20, top: 150, bottom: 80),
+          color: Colors.green,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Expanded(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.all(20),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SecondScreen()));
-                  },
-                  child: const Text(
-                    'Second Screen',
-                    style: TextStyle(
-                      fontSize: 10,
-                    ),
-                  ),
-                ),
+              Text(
+                "Hello, \nWelcome Back",
+                style: Theme.of(context).textTheme.headline1,
               ),
-              Expanded(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.all(20),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const HandGesture()));
-                  },
-                  child: const Text(
-                    'Hand Gesture',
-                    style: TextStyle(
-                      fontSize: 10,
+              Column(
+                children: [
+                  RaisedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SecondScreen()));
+                    },
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Text(
+                      "Facial Gestures",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Expanded(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.all(20),
+                  const SizedBox(
+                    height: 20,
                   ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ThirdScreen()));
-                  },
-                  child: const Text(
-                    'Third Screen',
-                    style: TextStyle(
-                      fontSize: 10,
+                  RaisedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const HandGesture()));
+                    },
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Text(
+                      "Hand Gestures",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ButtonTheme(
+                    minWidth: double.infinity,
+                    height: 60.0,
+                    child: RaisedButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const ThirdScreen()));
+                      },
+                      color: Color.fromARGB(106, 32, 156, 82),
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      child: const Text(
+                        "About",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               )
             ],
           ),
         ),
-      );
+      ),
+    );
+  }
 }
